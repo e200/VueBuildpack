0cab81af474504b3cbe7dbd2bb6046a37515d381
e200 <eleandro@inbox.ru>
2018-03-29 13:04:25 +0100

    Updated detect_test.sh

--- test/detect_test.sh
+++ test/detect_test.sh
@@ -5,6 +5,21 @@
 testDetectSrcFolder()
 {
   mkdir ${BUILD_DIR}/src
+  mkdir ${BUILD_DIR}/build
+  mkdir ${BUILD_DIR}/config
+
+  PACKAGE_JSON_CONTENT=`cat <<EOF
+  {
+    "dependencies": {
+      "vue": "^2.5.2"
+    }
+  }
+  EOF`
+
+  echo ${PACKAGE_JSON_CONTENT} > pakage.json
+  touch index.html
+
   detect
+
   assertAppDetected VueJS
 }
