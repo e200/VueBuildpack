a091cc3f0f83926b2bf8193d86a81aef85d7d2c8
e200 <eleandro@inbox.ru>
2018-03-29 13:55:34 +0100

    Added package.json text content

--- test/detect_test.sh
+++ test/detect_test.sh
@@ -8,7 +8,9 @@ testDetect()
   mkdir -p ${BUILD_DIR}/build
   mkdir -p ${BUILD_DIR}/config
 
-  touch ${BUILD_DIR}/package.json
+  PACKAGE_JSON_CONTENT=`echo "\"vue\": \"0.0.0\""
+
+  echo ${PACKAGE_JSON_CONTENT} > ${BUILD_DIR}/package.json
   touch ${BUILD_DIR}/index.html
 
   detect
